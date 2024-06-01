5737705269927f03e5f013114152bb517a1b42bd
KhadharThedevil <alyas.noob@hotmail.com>
2024-06-01 20:04:27 +0200

    edit the updaterequest

--- Data/Factories/CourseFactory.cs
+++ Data/Factories/CourseFactory.cs
@@ -54,38 +54,15 @@ public class CourseFactory
         {
             Id = request.Id,
             ImageUri = request.ImageUri,
-            ImageHeaderUri = request.ImageHeaderUri,
-            IsBestseller = request.IsBestseller,
-            IsDigital = request.IsDigital,
-            Categories = request.Categories,
             Title = request.Title,
-            Ingress = request.Ingress,
-            StarRating = request.StarRating,
-            Reviews = request.Reviews,
-            LikesInProcent = request.LikesInProcent,
             Likes = request.Likes,
             Hours = request.Hours,
             Authors = request.Authors?.Select(a => new AoutherEntity
             {
                 Name = a.Name,
             }).ToList(),
-            Prices = request.Prices == null ? null : new PricesEntity
-            {
-                Currency = request.Prices.Currency,
-                Price = request.Prices.Price,
-                Discount = request.Prices.Discount,
-            },
-            Content = request.Content == null ? null : new ContentEntity
-            {
-                Description = request.Content.Description,
-                Includes = request.Content.Includes,
-                ProgramDetails = request.Content.ProgramDetails?.Select(pd => new ProgramDetailItemEntity
-                {
-                    Id = pd.Id,
-                    Title = pd.Title,
-                    Description = pd.Description,
-                }).ToList()
-            }
+           
+            
         };
     }
 
